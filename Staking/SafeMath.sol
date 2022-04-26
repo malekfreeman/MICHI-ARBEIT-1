866d2c51ea7539748e4ed258354333c7af145d10
malekfreeman <100852922+malekfreeman@users.noreply.github.com>
2022-04-27 01:47:49 +0200

    Update SafeMath.sol

--- Staking/SafeMath.sol
+++ Staking/SafeMath.sol
@@ -2,8 +2,8 @@
  * @title Safe Math
  * @dev SafeMath contract
  *
- * @author - <MIDGARD TRUST>
- * for the Midgard Trust
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
  *
  * SPDX-License-Identifier: GNU GPLv2
  *
