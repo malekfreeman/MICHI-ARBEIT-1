344977b476de311312bf357c31703c541c467d93
malekfreeman <100852922+malekfreeman@users.noreply.github.com>
2022-04-27 01:04:38 +0200

    Update IERC20.sol

--- Staking/IERC20.sol
+++ Staking/IERC20.sol
@@ -2,8 +2,8 @@
  * @title Interface ERC20
  * @dev IERC20 contract
  *
- * @author - <MIDGARD TRUST>
- * for the Midgard Trust
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
  *
  * SPDX-License-Identifier: GNU GPLv2
  *
