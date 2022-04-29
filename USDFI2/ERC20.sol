da05fd7ef73ec5124e32d08e6ca988f2aeb0afb2
malekfreeman <100852922+malekfreeman@users.noreply.github.com>
2022-04-30 00:10:02 +0200

    Update ERC20.sol

--- USDFI2/ERC20.sol
+++ USDFI2/ERC20.sol
@@ -2,8 +2,8 @@
  * @title ERC20
  * @dev ERC20 contract
  *
- * @author - <MIDGARD TRUST>
- * for the Midgard Trust
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
  *
  * SPDX-License-Identifier: GNU GPLv2
  *
