fce71dd4579ae3852b316df73078e288ce8d60f0
malekfreeman <100852922+malekfreeman@users.noreply.github.com>
2022-04-30 13:03:48 +0200

    Update IERC20.sol

--- realminer/IERC20.sol
+++ realminer/IERC20.sol
@@ -2,8 +2,8 @@
  * @title Interface ERC20
  * @dev IERC20 contract
  *
- * @author - <MIDGARD TRUST>
- * for the Midgard Trust
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
  *
  * SPDX-License-Identifier: GNU GPLv2
  *
@@ -52,7 +52,7 @@ interface IERC20 {
      *
      * Returns a boolean value indicating whether the operation succeeded.
      *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * IMPORTANT: Be aware that changing an allowance with this method brings the risk
      * that someone may use both: the old and the new allowance by unfortunate
      * transaction ordering. One possible solution to mitigate this race
      * condition is to first reduce the spender's allowance to 0 and set the
