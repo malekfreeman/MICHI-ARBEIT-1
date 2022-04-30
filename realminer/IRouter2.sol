f6e773559fdde36bd68b8b42e86815c1b80c6432
malekfreeman <100852922+malekfreeman@users.noreply.github.com>
2022-04-30 13:17:06 +0200

    Update IRouter2.sol

--- realminer/IRouter2.sol
+++ realminer/IRouter2.sol
@@ -2,8 +2,8 @@
  * @title Interface Router
  * @dev IRouter2 contract
  *
- * @author - <MIDGARD TRUST>
- * for the Midgard Trust
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
  *
  * SPDX-License-Identifier: GNU GPLv2
  *
