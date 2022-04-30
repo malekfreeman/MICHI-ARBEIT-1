c3573fcae8d77a588964a4a896fc907aeaed9bb9
malekfreeman <100852922+malekfreeman@users.noreply.github.com>
2022-04-30 13:57:28 +0200

    Update SafeERC20.sol

--- realminer/SafeERC20.sol
+++ realminer/SafeERC20.sol
@@ -2,8 +2,8 @@
  * @title Safe ERC20
  * @dev SafeERC20 contract
  *
- * @author - <MIDGARD TRUST>
- * for the Midgard Trust
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
  *
  * SPDX-License-Identifier: GNU GPLv2
  *
