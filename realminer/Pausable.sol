78aae56076f160fdb201fb894d88bfdc5e28abd8
malekfreeman <100852922+malekfreeman@users.noreply.github.com>
2022-04-30 13:35:19 +0200

    Update Pausable.sol

--- realminer/Pausable.sol
+++ realminer/Pausable.sol
@@ -2,8 +2,8 @@
  * @title Pausable
  * @dev Pausable contract
  *
- * @author - <MIDGARD TRUST>
- * for the Midgard Trust
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
  *
  * SPDX-License-Identifier: GNU GPLv2
  *
